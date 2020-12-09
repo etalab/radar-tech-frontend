068c8e3dcc55ac30abba151f8ff6b237c32d6d13
Basile Simon <basile.simon@data.gouv.fr>
2020-12-09 17:25:52 +0100

    Extrait la query des métadonnées dans un Hook
    C'est réutilisable, c'est joli
    Voir: https://www.gatsbyjs.com/docs/use-static-query/#composing-custom-usestaticquery-hooks

--- src/components/design-systeme/components/Header.js
+++ src/components/design-systeme/components/Header.js
@@ -1,73 +1,57 @@
 import React from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { useSiteMetadata } from '../../utils/use-site-metadata';
+import { Link } from 'gatsby';
 
-export default function Header() {
+const Header = () => {
+  const { title, description } = useSiteMetadata();
   return (
-    <StaticQuery
-      query={graphql`
-        query HeadingQuery {
-          site {
-            siteMetadata {
-              title
-              description
-            }
-          }
-        }
-      `}
-      render={data => (
-        <header className="rf-header">
-          <div className="rf-container">
-            <div className="rf-header__body">
-              <div className="rf-header__brand">
-                <Link className="rf-logo" to="/" title="République française">
-                  <span className="rf-logo__title">
-                    République
-                    <br />
-                    française
-                  </span>
-                </Link>
-              </div>
-              <div className="rf-header__navbar">
-                <div className="rf-service">
-                  <Link
-                    className="rf-service__title"
-                    to="/"
-                    title={data.site.siteMetadata.title}
-                  >
-                    {data.site.siteMetadata.title}
-                  </Link>
-                  <p className="rf-service__tagline">
-                    {data.site.siteMetadata.description}
-                  </p>
-                </div>
-              </div>
-
-              <div className="rf-header__tools">
-                <div className="rf-shortcuts">
-                  <ul className="rf-shortcuts__list">
-                    <li className="rf-shortcuts__item">
-                      <Link
-                        to="/resultats/"
-                        className="rf-link rf-fi-lock-line rf-link--icon-left"
-                      >
-                        Résultats
-                      </Link>
-                    </li>
-                    <li className="rf-shortcuts__item">
-                      <Link
-                        to="/using-typescript/"
-                        className="rf-link rf-fi-lock-line rf-link--icon-left"
-                      >
-                        Typescript
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+    <header className="rf-header">
+      <div className="rf-container">
+        <div className="rf-header__body">
+          <div className="rf-header__brand">
+            <Link className="rf-logo" to="/" title="République française">
+              <span className="rf-logo__title">
+                République
+                <br />
+                française
+              </span>
+            </Link>
+          </div>
+          <div className="rf-header__navbar">
+            <div className="rf-service">
+              <Link className="rf-service__title" to="/" title={title}>
+                {title}
+              </Link>
+              <p className="rf-service__tagline">{description}</p>
             </div>
           </div>
-        </header>
-      )}
-    />
+
+          <div className="rf-header__tools">
+            <div className="rf-shortcuts">
+              <ul className="rf-shortcuts__list">
+                <li className="rf-shortcuts__item">
+                  <Link
+                    to="/resultats/"
+                    className="rf-link rf-fi-lock-line rf-link--icon-left"
+                  >
+                    Résultats
+                  </Link>
+                </li>
+                <li className="rf-shortcuts__item">
+                  <Link
+                    to="/using-typescript/"
+                    className="rf-link rf-fi-lock-line rf-link--icon-left"
+                  >
+                    Typescript
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
-}
+};
+
+export default Header;
