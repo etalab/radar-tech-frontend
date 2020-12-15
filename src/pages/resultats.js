bb6b7ce5c4271bf3a032eab88c608a281f081899
Basile Simon <basile.simon@data.gouv.fr>
2020-12-15 16:58:30 +0100

    Query quelques data points, render quelque chose de correct

--- src/pages/resultats.js
+++ src/pages/resultats.js
@@ -2,11 +2,60 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import { useStaticQuery, graphql } from 'gatsby';
+const d3 = Object.assign({}, require('d3-collection'));
+
+const groupBy = (arr, key) =>
+  d3
+    .nest()
+    .key(d => d[key])
+    .rollup(d => {
+      return { n: d.length, pct: ((100 * d.length) / arr.length).toFixed(1) };
+    })
+    .entries(arr)
+    .sort((a, b) => b.value.n - a.value.n);
+
 const ResultatsPage = () => {
+  const { radarTechTest } = useStaticQuery(graphql`
+    query myQuery {
+      radarTechTest {
+        answer {
+          gender
+          age
+        }
+      }
+    }
+  `);
+
+  const data = radarTechTest.answer;
+  const gender_keyed = groupBy(data, 'gender');
+  const age_keyed = groupBy(data, 'age');
+
   return (
     <Layout>
       <SEO title="Résultats" />
-      <h1>page des resultats</h1>
+      <h1>page des résultats</h1>
+      <p>
+        <b>{data.length}</b> résultats dans la DB.
+      </p>
+
+      <h4>Gender</h4>
+      <ul>
+        {gender_keyed.map((e, i) => (
+          <li key={i}>
+            {e.value.n} identifiant comme {e.key}s ({e.value.pct}%)
+          </li>
+        ))}
+      </ul>
+
+      <h4>Age</h4>
+      <ul>
+        {age_keyed.map((e, i) => (
+          <li key={i}>
+            {e.value.n} entre {e.key} ans ({e.value.pct}%)
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
