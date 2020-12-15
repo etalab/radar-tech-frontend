b0123c0056c7b0410315fc7e26b949e24e087a0d
Basile Simon <basile.simon@data.gouv.fr>
2020-12-15 15:51:47 +0100

    Simple page de résultats, sans data

---
+++ src/pages/resultats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ResultatsPage = () => {
+  return (
+    <Layout>
+      <SEO title="Résultats" />
+      <h1>page des resultats</h1>
+    </Layout>
+  );
+};
+
+export default ResultatsPage;
