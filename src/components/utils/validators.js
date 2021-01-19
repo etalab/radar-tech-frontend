a7694063b82e114b99ea2f76174e721b6354aaec
Basile Simon <basile.simon@data.gouv.fr>
2021-01-19 16:37:06 +0100

    Validateur pour verrouiller un peu la forme du questionnaire

--- src/components/utils/validators.js
+++ src/components/utils/validators.js
@@ -1,22 +1,46 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 // le format du questionnaire-test n'a pas vocation à être modifié trop
-// facilement. nous le contraignons donc au respect de ce schéma yup()
+// facilement. nous le contraignons donc au respect de ce schéma yup
 const schema = yup
   .object()
   .shape({
-    demo_genre: yup
-      .string()
-      .required()
-      .oneOf([
-        "Homme",
-        "Femme",
-        "Non-binaire, genderqueer, ou gender non-conforming",
-      ]),
+    autres_libs: yup.array().of(yup.string()),
+    connaissance_db: yup.array().of(yup.string()),
+    connaissance_db_expert: yup.array().of(yup.string()),
+    containers_bool: yup.string(),
+    containers_liste: yup.array().of(yup.string()),
+    'containers_list-Comment': yup.string(),
+    demo_administration: yup.string(),
     demo_age: yup.string(),
-    travail_activite: yup.string().oneOf(["Oui", "Non"]),
-    travail_activite_autre: yup.array().of(yup.string()),
+    demo_entree_admin: yup.string(),
+    demo_etudiant: yup.array().of(yup.string()),
+    demo_fonctions: yup.string(),
+    demo_plus_hautes_etudes: yup.string(),
+    demo_rythme: yup.string(),
+    demo_status: yup.string(),
+    demo_titre: yup.string(),
+    editeur_principal: yup.array().of(yup.string()),
     education_formation: yup.object(),
+    experience_programmation: yup.number(),
+    frameworks_web: yup.array().of(yup.string()),
+    language_expert: yup.array().of(yup.string()),
+    language_pas_expert: yup.array().of(yup.string()),
+    os_principal: yup.array().of(yup.string()),
+    plateformes_os: yup.array().of(yup.string()),
+    premiere_ligne_code: yup.number(),
+    profils_tech: yup.number(),
+    programmation_pro: yup.number(),
+    recherche_travail: yup.array().of(yup.string()),
+    recherche_travail_secteur: yup.array().of(yup.string()),
+    recherches_info: yup.array().of(yup.string()),
+    recherches_info_texte: yup.array().of(yup.string()),
+    satisfaction: yup.number(),
+    taille_structure: yup.string(),
+    travail_activite: yup.array().of(yup.string()),
+    travail_activite_autre: yup.array().of(yup.string()),
+    travail_bureau_domicile: yup.string(),
+    demo_genre: yup.string(),
     email: yup.string(),
   })
   .noUnknown();
