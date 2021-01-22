7b9d41f72bc54794c1acda54dbe8a71f29357792
Basile Simon <basilesimon@users.noreply.github.com>
2021-01-22 13:40:33 +0100

    Implémente un modèle de graphique responsive (#3)
    * Simple bar chart en HTML
    
    * Labelle les barres
    
    * Graphique de "petites personnes", responsive
    
    * Ajoute les données de l'age, facile
    
    * Petit graphique d'une distribution
    
    * Enlève la section "Age" – trop busy
    
    * Bundle de petits fixes, couleurs, problèmes

---
+++ src/components/HTMLBarChart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HTMLBarChart = props => (
+  <div
+    style={{
+      display: `flex`,
+      flexDirection: `row`,
+      marginBottom: `50px`,
+    }}
+  >
+    {props.data.map((e, i) => (
+      <div
+        key={i}
+        style={{
+          width: `${e.pct}%`,
+          height: `20px`,
+          backgroundColor: `${props.color(e.key)}`,
+          border: `1px solid #fff`,
+          borderRadius: `4px`,
+        }}
+      >
+        <span
+          style={{
+            textAlign: `center`,
+            color: `${props.color(e.key)}`,
+            transform: `translate(0,20px)`,
+            display: `flex`,
+            alignItems: `center`,
+            justifyContent: `center`,
+          }}
+        >
+          {e.pct}%<br />
+          {e.key}
+        </span>
+      </div>
+    ))}
+  </div>
+);
+
+export default HTMLBarChart;
