417fa6471e6b0e1e7410e0c3f931158a6daad407
Basile Simon <basile.simon@data.gouv.fr>
2021-03-23 11:01:53 +0100

    Clean up partial pour le moment

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,58 +1,53 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+// import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-import { SurveyComponent } from '../components/Survey';
+// import { SurveyComponent } from '../components/Survey';
 
-import buildSurveys from '../components/utils/assembleSurveys';
+// import buildSurveys from '../components/utils/assembleSurveys';
 
 function DesignersPage({ data }) {
-  const questionnaireData = buildSurveys(
-    data.allContentJson.nodes,
-    'questions communes',
-    'designers'
-  );
-
   return (
     <Layout>
       <SEO title="Home" />
-      <SurveyComponent questionnaireData={questionnaireData} />
+      <p>temporary page, y'a plus rien ici.</p>
+      {/* <SurveyComponent questionnaireData={questionnaireData} /> */}
     </Layout>
   );
 }
 
 export default DesignersPage;
 
-export const query = graphql`
-  query QuestionsDesign {
-    allContentJson(
-      filter: { key: { in: ["questions communes", "designers"] } }
-    ) {
-      nodes {
-        key
-        pages {
-          name
-          title
-          elements {
-            type
-            name
-            title
-            isRequired
-            colCount
-            visibleIf
-            hasOther
-            choices
-            validators {
-              type
-            }
-          }
-        }
-        showQuestionNumbers
-        title
-        completedHtml
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query QuestionsDesign {
+//     allContentJson(
+//       filter: { key: { in: ["questions communes", "designers"] } }
+//     ) {
+//       nodes {
+//         key
+//         pages {
+//           name
+//           title
+//           elements {
+//             type
+//             name
+//             title
+//             isRequired
+//             colCount
+//             visibleIf
+//             hasOther
+//             choices
+//             validators {
+//               type
+//             }
+//           }
+//         }
+//         showQuestionNumbers
+//         title
+//         completedHtml
+//       }
+//     }
+//   }
+// `;
