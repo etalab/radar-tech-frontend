5de16eb9b4771e41c65114e01f21d5ea29405185
audrey bramy <audrey.bramy@gmail.com>
2021-04-14 23:24:06 +0200

    remove useless config file

--- env/production.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    API_URL: "http://radartech-backend-test.app.etalab.studio/graphql",
-    API_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZyb250ZW5kIiwicGFzc3dvcmQiOiIzMDVlY2FmMmY3MWM3ZmQ3MDU2YmRlZDQ4MDNmMDhlY2JiYzcyOWQ5Yjk2NTJmMDhiMDcxM2I2YjI1MTFhNmRjIiwiaWQiOiI2MDc2OTgyMDk3ODg2MGJlMzAyNGM2ZDYiLCJpYXQiOjE2MTgzODUwNTB9.J_FBCXONuyCZ-JZxw9KV5JuTY8N7vHa5i2UBo6zj-Iw"
-}
