ff2e22e187aa459910af5778cfd177bfbce4fd20
audrey bramy <audrey.bramy@gmail.com>
2021-04-14 16:08:07 +0200

    load environment file in Survey.js

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -7,6 +7,12 @@ import { schema } from './utils/validators.js';
 //import questionnaire from './questionnaire.js';
 //import { isConstructorDeclaration } from 'typescript';
 
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 import SuccessComponent from './Success';
 const completedHtml = SuccessComponent();
 
