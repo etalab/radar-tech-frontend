974baf586f0bd41e6d42406e453cc9637f64bd4f
audrey bramy <audrey.bramy@gmail.com>
2021-04-14 18:23:17 +0200

    change path to env file

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -9,6 +9,7 @@ import { schema } from './utils/validators.js';
 
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(activeEnv)
 require("dotenv").config({
   path: `../../.env.${activeEnv}`,
 })
