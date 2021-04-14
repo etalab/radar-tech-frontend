70c3a8733378b8d884748aa2b312424cda91ac4b
audrey bramy <audrey.bramy@gmail.com>
2021-04-14 23:33:28 +0200

    correct env file path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 require("dotenv").config({
-  path: `./env/${activeEnv}.js`,
+  path: `.env.${activeEnv}`,
 })
 module.exports = {
   siteMetadata: {
