0f4d4bc80379d5ff08e3940a10aebe4873c5fc4c
Basile Simon <basile.simon@data.gouv.fr>
2021-09-17 11:33:40 +0200

    Regression dans le style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import "./css/404.scss";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -8,7 +7,7 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <section className="404">
+    <section style={{margin: `5rem 0` }}>
       <h1>Erreur 404: Page non trouvée</h1>
       <p>La page que vous cherchez ne peut être trouvée.</p>
       <Link to="/">Retour à la page d'accueil</Link>
