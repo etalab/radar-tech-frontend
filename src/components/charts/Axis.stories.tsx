abf804117e2d21baef286471d8545db9da3331e7
Basile Simon <basile.simon@data.gouv.fr>
2021-10-19 17:27:56 +0200

    Axes maintenant typés, supportent linéraire et catégorique

--- src/components/charts/Axis.stories.tsx
+++ src/components/charts/Axis.stories.tsx
@@ -1,30 +1,56 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { AxisLinear } from './Axis';
+import { Axis } from './Axis';
 import Layout from '../layout';
 
 export default {
-  title: 'Graphiques/Axe Linéaire',
-  component: AxisLinear,
-} as ComponentMeta<typeof AxisLinear>;
+  title: 'Graphiques/Axe',
+  component: Axis,
+} as ComponentMeta<typeof Axis>;
 
 const width = 400;
 
-const Template: ComponentStory<typeof AxisLinear> = args => (
+const Template: ComponentStory<typeof Axis> = args => (
   <React.Fragment>
     <Layout noFurniture={true}>
       <svg width={width} height="200" style={{ border: '1px dashed #ddd' }}>
         <g transform={`translate(20, 50)`}>
-          <AxisLinear {...args} />
+          <Axis {...args} />
         </g>
       </svg>
     </Layout>
   </React.Fragment>
 );
 
-export const ParDéfaut = Template.bind({});
-ParDéfaut.args = { parentWidth: width - 40, domain: [0, 100], axisPath: false };
+export const LinéaireParDéfaut = Template.bind({});
+LinéaireParDéfaut.args = {
+  parentWidth: width - 40,
+  domain: [0, 100],
+  axisPath: false,
+  scaleType: 'linear',
+};
 
-export const AvecLigne = Template.bind({});
-AvecLigne.args = { parentWidth: width - 40, domain: [0, 100], axisPath: true };
+export const LinéaireAvecLigne = Template.bind({});
+LinéaireAvecLigne.args = {
+  parentWidth: width - 40,
+  domain: [0, 100],
+  axisPath: true,
+  scaleType: 'linear',
+};
+
+export const CategoricalParDéfaut = Template.bind({});
+CategoricalParDéfaut.args = {
+  parentWidth: width - 40,
+  domain: ['Marié.e', 'Sans enfant', 'Sandwich', 'Mer Noire'],
+  axisPath: false,
+  scaleType: 'categorical',
+};
+
+export const CategoricalAvecLigne = Template.bind({});
+CategoricalAvecLigne.args = {
+  parentWidth: width - 40,
+  domain: ['Marié.e', 'Sans enfant', 'Sandwich', 'Mer Noire'],
+  axisPath: true,
+  scaleType: 'categorical',
+};
--- src/components/charts/Axis.tsx
+++ src/components/charts/Axis.tsx
@@ -4,33 +4,66 @@ const d3 = { ...scale };
 
 interface AxisProps {
   parentWidth: number;
-  domain: [number, number];
+  domain: any[];
+  scaleType: 'linear' | 'categorical';
   axisPath?: boolean;
 }
 
-export const AxisLinear = ({
+const pickScale = (scaleType: 'linear' | 'categorical', range: any[]) => {
+  if (scaleType === 'linear') {
+    return d3.scaleLinear().range(range);
+  } else {
+    return d3.scaleBand().rangeRound(range);
+  }
+};
+
+const getTicks = (
+  scale,
+  scaleType: 'linear' | 'categorical',
+  noOfTicks?: number
+): { value: number | string; xOffset: number }[] => {
+  if (scaleType === 'linear') {
+    return scale.ticks(noOfTicks).map((value: number) => ({
+      value,
+      xOffset: scale(value),
+    }));
+  } else {
+    return scale.domain().map((value: any) => ({
+      value,
+      xOffset: scale(value) + scale.bandwidth() / 2,
+    }));
+  }
+};
+
+const tickTransform = (scaleType: 'linear' | 'categorical') =>
+  `translate(-5px, 25px) ${scaleType === 'linear' ? 'rotate(-45deg)' : ''}`;
+
+const axisLine = (scaleType: 'linear' | 'categorical', range: number[]) => {
+  const offset = scaleType === 'linear' ? 6 : 0;
+  // prettier-ignore
+  return ['M', range[0], offset, 'v', -offset, 'H', range[1], 'v', offset].join(' ');
+};
+
+export const Axis = ({
   parentWidth,
   domain,
+  scaleType,
   axisPath,
 }: AxisProps) => {
   const range = [0, parentWidth];
   const ticks = useMemo(() => {
-    const xScale = d3.scaleLinear().domain(domain).rangeRound(range);
+    const xScale = pickScale(scaleType, range).domain(domain);
     const width = range[1] - range[0];
     const pixelsPerTick = 30;
     const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick));
-
-    return xScale.ticks(numberOfTicksTarget).map(value => ({
-      value,
-      xOffset: xScale(value),
-    }));
+    return getTicks(xScale, scaleType, numberOfTicksTarget);
   }, [domain, range]);
 
   return (
     <g>
       {axisPath && (
         <path
-          d={['M', range[0], 6, 'v', -6, 'H', range[1], 'v', 6].join(' ')}
+          d={axisLine(scaleType, range)}
           fill="none"
           stroke="currentColor"
         />
@@ -41,9 +74,9 @@ export const AxisLinear = ({
           <text
             key={value}
             style={{
-              fontSize: '10px',
+              fontSize: '11px',
               textAnchor: 'middle',
-              transform: 'translate(-5px,25px) rotate(-45deg)',
+              transform: tickTransform(scaleType),
             }}
           >
             {value}
